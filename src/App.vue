849482b7836ff09fd60bcff863301fa4e9eaaa31
btorresf <bruno.torres.fabila@gmail.com>
2023-12-08 15:03:44 -0600

    feat: add icons & rearrange styles

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import tranxportaLogo from '@/assets/traxporta-logo.png';
+</script>
 
 <template>
-  <v-app width="100vw" height="100vh">
-    <v-navigation-drawer></v-navigation-drawer>
-    <v-main class="d-flex justify-center main-content"></v-main>
+  <v-app>
+    <v-navigation-drawer rail rail-width="92" color="white">
+      <v-list-item
+        nav
+        width="52"
+        height="52"
+        class="my-9 mx-auto rounded-lg elevation-3">
+        <v-img :src="tranxportaLogo" />
+      </v-list-item>
+    </v-navigation-drawer>
+    <v-main class="d-flex justify-center main-content">
+      <v-sheet
+        :elevation="8"
+        rounded="xl"
+        class="d-flex flex-column justify-center my-auto"
+        height="45rem"
+        width="38.75rem">
+        <div class="text-h6 font-weight-bold text-center">Users</div>
+      </v-sheet>
+    </v-main>
   </v-app>
 </template>
 
 <style scoped lang="scss">
 .main-content {
   background-color: #f9f9f9;
+  border: 1px solid red;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import './assets/main.scss';
-
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import 'vuetify/styles';
+import './assets/main.scss';
 
 import App from './App.vue';
 
